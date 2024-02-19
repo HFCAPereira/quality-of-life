71267548e4c3165f14ad67d1fc675db7e034bb1b
HFCAPereira <138407107+HFCAPereira@users.noreply.github.com>
2024-02-19 18:03:05 +0000

    Create rdp.ps1

---
+++ rdp.ps1
@@ -0,0 +1,4 @@
+Invoke-Item "\\gueaspuiprd02\E$\Tomcat\webapps\identityiq\WEB-INF\lib"
+Invoke-Item "\\gueaspuiprd01\E$\Tomcat\webapps\identityiq\WEB-INF\lib"
+Invoke-Item "\\gueasptsprd01\E$\Tomcat\webapps\identityiq\WEB-INF\lib"
+Invoke-Item "\\gueasptsprd02\E$\Tomcat\webapps\identityiq\WEB-INF\lib"
